f66c053d03e4e67f01d412e233f6ec2f464c4769
Robert Buna <robertbuna@yahoo.com>
2019-10-27 20:25:29 -0400

    try two

--- monthly_transactions.js
+++ monthly_transactions.js
@@ -4,26 +4,16 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
 
 	const params = {
-	TableName: process.env.tableName,
-	Key: {
-		transactionId: event.pathParameters.id,
-		transactionDate: event.pathParameters.date,
-		userId: event.requestContext.identity.cognitoIdentityId
-		}
-	};
+		TableName: process.env.tableName,
+		KeyConditionExpression: "yearMonth = :yearMonth",
+		ExpressionAttributeValues: {":yearMonth": event.pathParameters.yearMonth}
+		};
 
 	try {
-		const result = await dynamoDbLib.call("get",params);
-
-		if (result.Item) {
-			// Return the retrieved item
-			return success(result.Item);
-			}
-		else {
-			return failure({ status: false, error: "Item not found." });
-			}
+		const result = await dynamoDbLib.call("query",params);
+		return success(result.Items);
 		}
 	catch (e) {
-		return failure({ status: e });
+		return failure({ status: {params, e} });
 		}
 	}
